f8665feeee9c0bf7c99c70bd570408175b06c78a
Jayaram <jayaramvenkatanarayanan@live.com>
2023-11-19 11:31:27 +0530

    refactor: colors modified

--- lua/plugins/colourscheme.lua
+++ lua/plugins/colourscheme.lua
@@ -6,7 +6,11 @@ return {
     priority = 1000,
     config = function()
       require("everforest").setup({
-        -- Your config here
+        -- My config here
+        background = "hard",
+        transparent_background_level = 2,
+        italics = true,
+        float_style = "bright",
       })
     end,
   },
